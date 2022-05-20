5b0898872fd16147ef781a3fde9745ef98040c97
Juan Otálora <19978031+juanoa@users.noreply.github.com>
2022-05-20 17:32:19 +0200

    Refactor repository

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -3,12 +3,12 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
@@ -16,8 +16,11 @@ firebase.initializeApp(firebaseConfig)
 const db = firebase.firestore()
 const auth = firebase.auth()
 
+const firstLevelCollection = "profitchart";
+
 export {
-    db,
-    auth,
-    firebase
+  db,
+  auth,
+  firebase,
+  firstLevelCollection
 }
--- src/pages/accounts/CreateAccountPage.jsx
+++ src/pages/accounts/CreateAccountPage.jsx
@@ -5,7 +5,7 @@ import {useForm} from "../../hooks/useForm";
 import {startCreateAccount} from "../../actions/accounts";
 import PageLayout from "../../layout/PageLayout";
 import {accountColors, accountTypes} from "../../config/data/account";
-import {currencies} from "../../config/data/currencies";
+import {currencies} from "../../config/data/currency";
 
 export const CreateAccountPage = () => {
 
