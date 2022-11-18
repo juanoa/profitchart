c2df710c9556bb569e9b7cd755fccb09985abbb8
Juan Otálora <19978031+juanoa@users.noreply.github.com>
2022-11-18 17:45:27 +0100

    Delete old routing method and auth

--- src/ui/router/AppRouter.tsx
+++ src/ui/router/AppRouter.tsx
@@ -1,41 +1,37 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch,} from "react-router-dom";
-
-import {LoginPage} from "../pages/authentication/LoginPage";
-import {DashboardRouter} from "./DashboardRouter";
-import {Toast} from "../layout/Toast";
-import {Loading} from "../layout/Loading";
-import {PrivateRoute} from "./PrivateRoute";
-import {PublicRoute} from "./PublicRoute";
-import {useAuthenticationContext} from "../contexts/AuthenticationContext";
+import {BrowserRouter as Router, Redirect, Route, Switch,} from "react-router-dom";
+import {Navbar} from "../layout/Navbar";
+import {routes} from "./routes";
 
 export const AppRouter = () => {
 
-  const {isLoading, isLoggedIn} = useAuthenticationContext();
-
-  if (isLoading) {
-    return (
-      <Loading/>
-    )
-  }
 
   return (
     <Router>
-      <div>
-        <Switch>
-          <PublicRoute
-            exact
-            path='/login'
-            component={LoginPage}
-            isAuthenticated={isLoggedIn}
-          />
-          <PrivateRoute
-            path='/'
-            component={DashboardRouter}
-            isAuthenticated={isLoggedIn}
-          />
-        </Switch>
-      </div>
+      <Switch>
+        <div className="wrapper">
+          <Navbar/>
+          <div className="main-panel">
+            <div className="content pb-5">
+              <Switch>
+                {
+                  routes.map(({path, PageComponent}) => (
+                    <Route
+                      exact
+                      key={path}
+                      path={path}
+                      component={PageComponent}
+                    />
+                  ))
+                }
+                <Redirect
+                  to='/'
+                />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </Switch>
     </Router>
   );
 };
